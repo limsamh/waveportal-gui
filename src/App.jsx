527a7663ab7bcf101efb5fa1e05a873454690f1f
Salim Igué <salim.igue@yahoo.ca>
2022-04-16 12:05:30 +0000

    Adding wave option

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,99 @@
-import React, { useEffect, useState } from "react";
-import "./App.css";
+import React, { useEffect, useState } from 'react';
+import { ethers } from 'ethers';
+import './App.css';
+import abi from './utils/WavePortal.json';
+import 'dotenv/config';
+
 
 const App = () => {
-  const [currentAccount, setCurrentAccount] = useState("");
+	const [currentAccount, setCurrentAccount] = useState('');
+	/**
+	 * Holds the contract address after deploy!
+	 */
+	const contractAddress = process.env.CONTRACT_ADDRESS;
+	const contractABI = abi.abi;
 
-  const checkIfWalletIsConnected = async () => {
-    try {
-      const { ethereum } = window;
+	const checkIfWalletIsConnected = async () => {
+		try {
+			const { ethereum } = window;
 
-      if (!ethereum) {
-        console.log("Make sure you have metamask!");
-        return;
-      } else {
-        console.log("We have the ethereum object", ethereum);
-      }
+			if (!ethereum) {
+				console.log('Make sure you have metamask!');
+				return;
+			} else {
+				console.log('We have the ethereum object', ethereum);
+			}
 
-      const accounts = await ethereum.request({ method: "eth_accounts" });
+			const accounts = await ethereum.request({ method: 'eth_accounts' });
 
-      if (accounts.length !== 0) {
-        const account = accounts[0];
-        console.log("Found an authorized account:", account);
-        setCurrentAccount(account);
-      } else {
-        console.log("No authorized account found")
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  }
+			if (accounts.length !== 0) {
+				const account = accounts[0];
+				console.log('Found an authorized account:', account);
+				setCurrentAccount(account);
+			} else {
+				console.log('No authorized account found');
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-  /**
-  * Connect to our wallet
-  */
-  const connectWallet = async () => {
-    try {
-      const { ethereum } = window;
+	/**
+	 * Connect to our wallet
+	 */
+	const connectWallet = async () => {
+		try {
+			const { ethereum } = window;
 
-      if (!ethereum) {
-        alert("Get MetaMask!");
-        return;
-      }
+			if (!ethereum) {
+				alert('Get MetaMask!');
+				return;
+			}
 
-      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+			const accounts = await ethereum.request({
+				method: 'eth_requestAccounts'
+			});
 
-      console.log("Connected", accounts[0]);
-      setCurrentAccount(accounts[0]);
-    } catch (error) {
-      console.log(error)
-    }
-  }
+			console.log('Connected', accounts[0]);
+			setCurrentAccount(accounts[0]);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	const wave = async () => {
+		try {
+			const { ethereum } = window;
+			if (ethereum) {
+				const provider = new ethers.providers.Web3Provider(ethereum);
+				const signer = provider.getSigner();
+				const wavePortalContract = new ethers.Contract(
+					contractAddress,
+					contractABI,
+					signer
+				);
+
+				let count = await wavePortalContract.getTotalWaves();
+				console.log('Retrieved total wave count...', count.toNumber());
+
+				/*
+        * Execute the actual wave from the smart contract
+        */
+				const waveTxn = await wavePortalContract.wave();
+				console.log('Mining...', waveTxn.hash);
+
+				await waveTxn.wait();
+				console.log('Mined -- ', waveTxn.hash);
+
+				count = await wavePortalContract.getTotalWaves();
+				console.log('Retrieved total wave count...', count.toNumber());
+			} else {
+				console.log("Ethereum object doesn't exist!");
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
 	/*
   * Runs our function when the page loads.
@@ -57,32 +102,30 @@ const App = () => {
 		checkIfWalletIsConnected();
 	}, []);
 
-  return (
-    <div className="mainContainer">
-      <div className="dataContainer">
-        <div className="header">
-        👋 Hey there!
-        </div>
+	return (
+		<div className="mainContainer">
+			<div className="dataContainer">
+				<div className="header">👋 Hey there!</div>
 
-        <div className="bio">
-          I am Salim, connect your Ethereum wallet and wave at me!
-        </div>
+				<div className="bio">
+					I am Salim, connect your Ethereum wallet and wave at me!
+				</div>
 
-        <button className="waveButton" onClick={null}>
-          Wave at Me
-        </button>
+				<button className="waveButton" onClick={wave}>
+					Wave at Me
+				</button>
 
-        {/*
+				{/*
         * If there is no currentAccount render this button
         */}
-        {!currentAccount && (
-          <button className="waveButton" onClick={connectWallet}>
-            Connect Wallet
-          </button>
-        )}
-      </div>
-    </div>
-  );
+				{!currentAccount && (
+					<button className="waveButton" onClick={connectWallet}>
+						Connect Wallet
+					</button>
+				)}
+			</div>
+		</div>
+	);
 };
 
 export default App;
